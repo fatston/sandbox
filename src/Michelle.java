f1123f789071c59c700fe72cc5b72f23c072e58b
fatston <cliftonkor@gmail.com>
2022-12-16 11:51:44 +0800

    remove comment

--- src/Michelle.java
+++ src/Michelle.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
-// test
+
 public class Michelle {
     public static void main(String[] args) {
         System.out.println("Hello World");
